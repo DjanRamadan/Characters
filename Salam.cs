bf1e3409f991f9681c9b069087548d97f80c6474
Djan <124775935+DjanRamadan@users.noreply.github.com>
2023-03-01 12:36:35 +0200

    Add files via upload

---
+++ Salam.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Enter character1: ");
+var ch1 = Convert.ToString(Console.ReadLine());
+Console.Write("Enter character2: ");
+var ch2 = Convert.ToString(Console.ReadLine());
+Console.Write("Enter character3: ");
+var ch3 = Convert.ToString(Console.ReadLine());
+
+Console.WriteLine(ch1 + ch2 + ch3);
